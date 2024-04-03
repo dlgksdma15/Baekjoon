a9133c4c06c291122265073850cadd43067f69d3
dlgksdma15 <161609129+dlgksdma15@users.noreply.github.com>
2024-04-03 16:31:09 +0900

    [Bronze III] Title: 최소, 최대, Time: 1696 ms, Memory: 318228 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] arsg){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i= 0;i < n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for(int i = 0; i < n; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        int min = arr[0];
+        for(int i = 0;i < n;i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        System.out.print(min + " " + max);
+
+    }
+}
