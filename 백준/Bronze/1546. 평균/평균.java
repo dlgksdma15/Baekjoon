a31ee1526700f68190e2f097879b709c4db93cda
dlgksdma15 <161609129+dlgksdma15@users.noreply.github.com>
2024-04-06 16:50:32 +0900

    [Bronze I] Title: 평균, Time: 364 ms, Memory: 23220 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -6,18 +6,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         double[] arr = new double[n];
-        for(int i = 0;i < n;i++){
+        for(int i = 0; i < n;i++){
             arr[i] = sc.nextDouble();
         }
-        double temp = 0;
-        double sum = 0;
         Arrays.sort(arr);
-        for(int i = 0; i < arr.length;i++){
-            temp = (arr[i] / arr[arr.length-1]) * 100.0;
-//            System.out.println(temp);
-            sum = sum + temp / arr.length;
-
+        double sum = 0;
+        double tmp = 0;
+        for(int i = 0;i < n;i++){
+            sum = (arr[i] / arr[arr.length-1]) * 100.0;
+            tmp += sum/n;
         }
-        System.out.println(sum);
+        System.out.println(tmp);
     }
 }
