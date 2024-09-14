c47af7760f4127a2904e7e42278ff76a25b4ca3e
dlgksdma15 <161609129+dlgksdma15@users.noreply.github.com>
2024-09-14 20:55:01 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 516 ms, Memory: 36564 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class Main {
@@ -5,21 +6,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int row = sc.nextInt();
         int col = sc.nextInt();
-        int[][] arr = new int[row][col];
+
         int[][] arr1 = new int[row][col];
-        for(int i = 0;i < row;i++){
-            for(int j = 0;j < col;j++){
-              arr[i][j] = sc.nextInt();
-            }
-        }
-        for(int i = 0;i < row;i++){
-            for(int j = 0;j < col;j++){
+        int[][] arr2 = new int[row][col];
+        int[][] sumarr = new int[row][col];
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
                 arr1[i][j] = sc.nextInt();
             }
         }
-        for(int i = 0;i < row;i++){
-            for(int j = 0;j < col;j++){
-                System.out.print(arr[i][j] + arr1[i][j] + " ");
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                System.out.print(arr1[i][j] + arr2[i][j]);
+                System.out.print(" ");
             }
             System.out.println();
         }
