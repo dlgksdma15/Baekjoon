346a46306071295e684e8362b5af244aad3d805f
dlgksdma15 <161609129+dlgksdma15@users.noreply.github.com>
2024-09-23 00:27:50 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 112 ms, Memory: 15892 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,30 +1,23 @@
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int[] arr = new int[26];
-		
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = -1;
-		}
- 
-		String S = br.readLine();
- 
-		for(int i = 0; i < S.length(); i++) {
-			char ch = S.charAt(i);
-    
-			if(arr[ch - 'a'] == -1) {	// arr 원소 값이 -1 인 경우에만 초기화
-				arr[ch - 'a'] = i;
-			}
-		}
- 
-		for(int val : arr) {	// 배열 출력
-			System.out.print(val + " ");
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String inputstr = reader.readLine(); // baekjoon
+        String str = "abcdefghijklmnopqrstuvwxyz";
+        int[] arr = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+        for(int i = 0; i < inputstr.length();i++){
+            for(int j = 0; j < str.length();j++){
+                if(inputstr.charAt(i) == str.charAt(j) && arr[j] == -1){ // b == b
+                    arr[j] = i;
+                }
+            }
+        }
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
 }
