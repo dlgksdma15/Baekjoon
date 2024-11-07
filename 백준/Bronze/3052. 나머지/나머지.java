60a3301ea7acc78c06f7a796bb842e23a8b54720
dlgksdma15 <161609129+dlgksdma15@users.noreply.github.com>
2024-11-07 12:31:09 +0900

    [Bronze II] Title: 나머지, Time: 176 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,14 +1,25 @@
-import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        HashSet<Integer> h = new HashSet<Integer>();
-        for(int i = 0; i < 10;i++){
-            h.add(sc.nextInt() % 42);
-        }
-        System.out.println(h.size());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int count = 0;
 
+        for(int i = 0; i < 10;i++){
+            arr[i] = sc.nextInt() % 42;
+        }
+        for(int i = 0; i < 10;i++){
+            int tmp = 0; //
+            for(int j = i + 1; j < 10;j++){
+                if(arr[i] == arr[j]){ // 비교해서 같으면 tmp++
+                    tmp++;
+                }
+            }
+            if(tmp == 0){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
